b1191b5c12000bf4803d61a51a32b7b2aca59a2a
emrekunt <emre@kunt.dev>
2022-12-02 16:30:03 +0300

    image resizer has been added and code clean up

---
+++ pix4ml/Libs/AppConstant.swift
@@ -0,0 +1,20 @@
+//
+//  AppConstant.swift
+//  pix4ml
+//
+//  Created by Emre Kunt on 2.12.2022.
+//  Copyright © 2022 Keyboarder Co. All rights reserved.
+//
+
+import Foundation
+
+struct AppConstant {
+    
+    static let directoryName                = "pix4ML"
+    
+    
+    struct Notifications {
+        static let capturePhotoClicked      = "capturePhotoClicked"
+    }
+    
+}
